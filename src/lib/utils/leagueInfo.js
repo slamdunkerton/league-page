01ef6db4708cd8909b1203698cb95b79e286fe42
slamdunkerton <101852119+slamdunkerton@users.noreply.github.com>
2022-03-18 14:28:56 +1000

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -25,28 +25,28 @@ https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#ii-adding
 export const managers = [
     {
     "roster": 1,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
-    "name":Nathan McGrath,
-    "location":Brisbane,Australia, // (optional)
-    "bio": Lorem ipsum...,
+    "name":"Nathan McGrath",
+    "location":"Brisbane", // (optional)
+    "bio": "Lorem ipsum...",
     "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
-    "favoriteTeam": buf,
-    "mode": Dynasty,
+    "favoriteTeam": "buf",
+    "mode": "Dynasty",
     "rival": {
-      name: Josh Fake Champ Damen- Philly Specials, // Can be anything (usually your rival's name)
+      name: "Josh Fake Champ Damen- Philly Specials", // Can be anything (usually your rival's name)
       link: 3, // manager array number within this array, or null to link back to all managers page
       image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
     },
     "favoritePlayer": 2212, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
-    "valuePosition": WR, 
-    "rookieOrVets": Vets,
-    "philosophy": Flog the cunts,
-    "tradingScale": 10 
+    "valuePosition": "WR", 
+    "rookieOrVets": "Vets",
+    "philosophy": "Picks don't help you win",
+    "tradingScale": 8 
     "preferredContact": "Text", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
     },
     {
     "roster": 2,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
-    "name": "Your Name",
-    "location": "Brooklyn", // (optional)
+    "name": "Christiaan Dowling",
+    "location": "Brisbane", // (optional)
     "bio": "Lorem ipsum...",
     "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
     "favoriteTeam": "nyj", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
@@ -77,7 +77,7 @@ export const managers = [
       image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
     },
     "favoritePlayer": 1426, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
-    "valuePosition": RB, // (optional) Favorite position (QB, WR, RB, TE, etc.)
+    "valuePosition": "RB", // (optional) Favorite position (QB, WR, RB, TE, etc.)
     "rookieOrVets": "'Rookies'", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
     "philosophy": "Your fantasy team's philosophy",
     "tradingScale": 8, // 1 - 10
