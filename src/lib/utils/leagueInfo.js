a631b85cd6571087d5e28580f1b821b3a9f10e57
slamdunkerton <101852119+slamdunkerton@users.noreply.github.com>
2022-03-18 14:23:53 +1000

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -83,6 +83,26 @@ export const managers = [
     "tradingScale": 8, // 1 - 10
     "preferredContact": "WhatsApp", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
     },
+    {,
+      "roster": 4,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
+    "name": "Your Name",
+    "location": "Brooklyn", // (optional)
+    "bio": "Lorem ipsum...",
+    "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
+    "favoriteTeam": "sea", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+    "mode": "'Rebuild'", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
+    "rival": {
+      name: "Nathan McGrath", // Can be anything (usually your rival's name)
+      link: 6, // manager array number within this array, or null to link back to all managers page
+      image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+    },
+    "favoritePlayer": 1426, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
+    "valuePosition": RB, // (optional) Favorite position (QB, WR, RB, TE, etc.)
+    "rookieOrVets": "'Rookies'", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
+    "philosophy": "Your fantasy team's philosophy",
+    "tradingScale": 8, // 1 - 10
+    "preferredContact": "WhatsApp", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
+    }, 
   ]
   
   
