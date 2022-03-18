dd0df76287c02e4fd6306febc70490f66430e9e4
slamdunkerton <101852119+slamdunkerton@users.noreply.github.com>
2022-03-18 14:05:08 +1000

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -25,21 +25,21 @@ https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#ii-adding
 export const managers = [
     {
     "roster": 1,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
-    "name": Nathan McGrath,
-    "location": Brisbane,Australia, // (optional)
+    "name": "Nathan McGrath",
+    "location": "Brisbane,Australia", // (optional)
     "bio": "Lorem ipsum...",
     "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
-    "favoriteTeam": buf
-    "mode": Win Now
+    "favoriteTeam": "buf"
+    "mode": "Win Now"
     "rival": {
-      name: Josh Fake Champ Damen- Philly Specials, // Can be anything (usually your rival's name)
+      name: "Josh Fake Champ Damen- Philly Specials", // Can be anything (usually your rival's name)
       link: 3, // manager array number within this array, or null to link back to all managers page
       image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
     },
     "favoritePlayer": 2212, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
-    "valuePosition": WR, 
-    "rookieOrVets": Vets,
-    "philosophy": Flog the cunts,
+    "valuePosition": "WR", 
+    "rookieOrVets": "Vets",
+    "philosophy": "Flog the cunts",
     "tradingScale": 10 
     "preferredContact": "Text", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
     },
@@ -69,8 +69,7 @@ export const managers = [
     "location": "Brooklyn", // (optional)
     "bio": "Lorem ipsum...",
     "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
-    "fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
-    "favoriteTeam": "nyj", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+    "favoriteTeam": "phi", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
     "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
     "rival": {
       name: "Rival", // Can be anything (usually your rival's name)
